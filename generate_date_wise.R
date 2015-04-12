a993c69d0fb2a9528f494fe4eebdeed57e2f7ffe
Amey Jadiye <ameyjadiye@gmail.com>
2015-04-12 12:50:41 +0530

    moving results to ouptut dir

--- generate_date_wise.R
+++ generate_date_wise.R
@@ -2,7 +2,7 @@
 
 mydata <- read.table("__data_for_R", sep="^")
 
-png(filename="./date_wise_chart.png")
+png(filename="./output/date_wise_chart.png")
 par(mar=c(5,8,4,2))
 betterDates <- format(as.Date(mydata$V1,"%b %d, %Y, %I:%M %p"),"%b - %Y")
 barplot(sort(table(betterDates)),main="Date wise chart",las=2,cex.names = 0.8,font.lab=2,ylab="Number of messages")
--- generate_member_wise.R
+++ generate_member_wise.R
@@ -2,7 +2,7 @@
 
 mydata <- read.table("__data_for_R", sep="^")
 
-png(filename="./member_wise_chart.png")
+png(filename="./output/member_wise_chart.png")
 par(las=2)
 par(mar=c(5,8,4,2))
 barplot(sort(table(mydata$V2)),main="Member wise chart",horiz=TRUE,cex.names = 0.8,font.lab=2,xlab="Number of messages")
